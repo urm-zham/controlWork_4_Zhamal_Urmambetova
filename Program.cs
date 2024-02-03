5161fb1e3459f32cafbb934e960f4634f24d1661
Zhamal Urmambetova <acerchrom1217@gmail.com>
2024-02-03 13:53:21 +0600

    Level 1.2. Added limit on user's parrot age (0-50)

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ parrots.PrintAll();
 string userParrotName = UserUtil.AskUser<string>("Please enter new parrot's name:");
 int userParrotAge = UserUtil.AskUser<int>("Please enter new parrot's age:");
 
+while (!(userParrotAge >= 0 && userParrotAge <= 50))
+{
+    Console.WriteLine("Enter age between 0 and 50!");
+    userParrotAge = UserUtil.AskUser<int>("Please enter new parrot's age:");
+}
+
 parrots.Add(new Parrot(userParrotName, userParrotAge));
 
 parrots.PrintAll();
